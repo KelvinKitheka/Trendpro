5f26a082ae06a8459fa52fa2af8c0c605b8b2f8f
Kelvin Kitheka <kelvinkithekaofficial@gmail.com>
2026-05-20 17:51:01 +0300

    feat(models): Add Application model with fields for tracking, applicant details, and status

--- workflow/models.py
+++ workflow/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Application(models.Model):
+    tracking_number = models.CharField(max_length=20, unique=True)
+    applicant_name = models.CharField(max_length=50)
+    applicant_email = models.EmailField()
+    company_name = models.CharField(max_length=50)
+    APPLICATION_CHOICES = [
+        ('recordation', 'Recordation'),
+        ('renewal', 'Renewal'),
+        ('change_of_ownership', 'Change of ownership'),
+        ('change_of_name', 'Change of name'),
+        ('discontinuation', 'Discontinuation'),
+
+    ]
+    application_type = models.CharField(max_length=50, choices=APPLICATION_CHOICES)
+    description = models.TextField()
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('submitted', 'Submitted'),
+        ('under_review', 'Under Review'),
+        ('need_more_info', 'Need more Information'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    ]
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='draft')
+    reviewer_comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    submitted_at = models.DateTimeField(null=True, blank=True)
+    reviewed_at = models.DateTimeField(null=True, blank=True)
