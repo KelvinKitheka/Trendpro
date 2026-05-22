fc7774c527b84db1bde282fb556ebe48b8e6266b
Kelvin Kitheka <kelvinkithekaofficial@gmail.com>
2026-05-22 15:31:59 +0300

    feat: Register Application model in admin

--- backend/TrendPro/workflow/admin.py
+++ backend/TrendPro/workflow/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Application
 
 # Register your models here.
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['tracking_number', 'applicant_name', 'company_name', 'status', 'created_at']
+    list_filter = ['status', 'application_type']
+    search_fields = ['tracking_number', 'applicant_name', 'company_name']
