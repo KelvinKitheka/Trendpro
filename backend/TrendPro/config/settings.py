20f891b4fe31679fa9ad04834167b6b7b15bdc7b
Kelvin Kitheka <kelvinkithekaofficial@gmail.com>
2026-05-22 12:03:15 +0300

    fix(settings): Correct casing of CorsMiddleware

--- backend/TrendPro/config/settings.py
+++ backend/TrendPro/config/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.corsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
